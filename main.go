16e7e557eb4e5471c2cab2f0a9b7a571e63bab4f
yuzhengguang <yuzhengguang@shining3d.com>
2022-06-14 09:40:58 +0800

    增加一段代码

--- main.go
+++ main.go
@@ -89,17 +89,29 @@ func main() {
 
 func index() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-			return
+		// if r.URL.Path != "/" {
+		// 	http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		// 	return
+		// }
+		if r.URL.Path == "/a" {
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			w.Header().Set("X-Content-Type-Options", "nosniff")
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintln(w, "Hello, aaaaaaaaaaaaaa!")
 		}
+		if r.URL.Path == "/b" {
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			w.Header().Set("X-Content-Type-Options", "nosniff")
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintln(w, "Hello, bbbbbbbbbbbbb!")
+		}
+
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintln(w, "Hello, World!")
 	})
 }
-
 
 
 func healthz() http.Handler {
